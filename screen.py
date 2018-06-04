36af0fbd94c31e7788fb44e7b0c82b4d3dc3bf4a
073085359 <39631904+073085359@users.noreply.github.com>
2018-06-04 13:53:59 -0400

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,8 +1,21 @@
 import pygame
-pygame.init
+import math
+import stockclass
+pygame.init()
+#INITIALIZED MODULES
+
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+WHITE = (248,248,255)
+
+#COLOURS
+
 
 win = pygame.display.set_mode((1200,800))
-win.fill((0,0,0))
+win.fill(WHITE)
+test = stockclass.stock(win,(300,300,200,200),"Automax",BLACK,0,0)
+test.createdatadisplay(GREEN)
+test.createvisualdata(3)
 pygame.display.update()
-
-#asdaksdbbaujwbdbakjdajbkd
