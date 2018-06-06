010b38c4652b3a646545a14ca5e31de562eb89fb
073085359 <39631904+073085359@users.noreply.github.com>
2018-06-06 14:48:03 -0400

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import stockclass
+import random
 pygame.init()
 #INITIALIZED MODULES
 
@@ -14,17 +15,62 @@ WHITE = (248,248,255)
 
 
 win = pygame.display.set_mode((1200,800))
+ingame = False
+inmenu = True
 win.fill(WHITE)
+#WINDOW AND WHILE LOOP VARIABLES
 
 ## Creates the visual stock data##
-test = stockclass.stock(win,(300,200,650,300),"Automax",BLACK,0,0)
-test.createdatadisplay(GREEN)
-test.createvisualdata(3)
-test.createaxis()
+
+weeklyoutcome = [] #empty list that will be used for the stock data
+for i in range(80): #Creates a list of 80 integers with values that range from -100 to 100
+    i = random.randrange(-100,100)
+    weeklyoutcome.append(i) #appends each i character to the list
+
 ##------------------------------##
+while inmenu:
+    win.fill(WHITE)
+    buttontest =  stockclass.Button(win,(60,550,300,100))
+    for i in range(3):
+        buttontest.createbutton(440,550 - i*200)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            setposition = pygame.mouse.get_pos()
+            print(setposition)
+            buttontest.buttonclick(440,550,setposition)
+            buttontest.buttonclick(440,350,setposition)
+            buttontest.buttonclick(440,150,setposition)
+##            if (buttontest.buttonclick(60,550,setposition)) == True :
+##                inmenu = False
+##                ingame = True
+    pygame.display.update()
 
-##Creates the Buttons for the screen##
-buttontest =  stockclass.Button(win,(60,100,200,200))
-buttontest.createbutton()
+    
+while ingame:
 
-pygame.display.update()
+    test = stockclass.stock(win,(500,100,650,300),"Automax",BLACK,0,0)
+    test.createdatadisplay(GREEN)
+    test.createvisualdata(weeklyoutcome)
+    test.createaxis()
+    pygame.display.update()
+
+
+    ##Creates the Buttons for the screen##
+    buttontest =  stockclass.Button(win,(60,550,300,100))
+    for i in range(3):
+        buttontest.createbutton(60,550 - i*200)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            setposition = pygame.mouse.get_pos()
+            print(setposition)
+            buttontest.buttonclick(60,550,setposition)
+            buttontest.buttonclick(60,350,setposition)
+            buttontest.buttonclick(60,150,setposition)
+
+
+        
+            
+    ##----------------------------------##
+
+
+    pygame.display.update()
