f2609480424d3570ae869918322d7972d6ab253c
ilyat <ilya.tkachov@hotmail.com>
2018-06-07 14:24:03 -0400

    Add files via upload

--- class_getindex.py
+++ class_getindex.py
@@ -4,26 +4,30 @@ import pandas_datareader
 from class_stockdata import *
 #from pandas_datareader import data, wb
 from datetime import datetime
-#nasdaq = [["NVDA",[["2017-11-08",1,2,200],["2017-12-08",2,3,201],["2017-13-08",3,4,202]]],["TSLA",[["2017-11-08",1,2,200],["2017-12-08",2,3,201]]]
-nasdaq = pandas_datareader.nasdaq_trader.get_nasdaq_symbols()
+
+nasdaqfile = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
+nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"])
 #pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(path_or_buf="E:\Comp Sci\final project\_index\nasdaq\symbols.txt")
 shr = "NVDA"
-a = pandas_datareader.robinhood.RobinhoodQuoteReader({shr}).read().to_string()#str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),cols="last_trade_price")#header=False,index=False)
-#b = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),index_col=0)
+a = pandas_datareader.robinhood.RobinhoodQuoteReader({shr}).read().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),header=False,index=False)#str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),cols="last_trade_price")#header=False,index=False)
+b = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),index_col=0) #usecols=['last_trade_price','updated_at']
 #pandas.to_string -------<<<<<<<< USE INSTEAD AT http://pandas.pydata.org/pandas-docs/version/0.12/io.html
 
-
-
-print(a)
 #print(b.index[7])
 #print(b.index[12])
-#print(b.ix[7])
-#b = pandas.read_csv(a.))
-#test = pandas_datareader.robinhood.RobinhoodQuoteReader("NVDA")
-#pandas.read_csv(pandas_datareader.robinhood.RobinhoodQuoteReader("NVDA").read().to_csv())
-time = print() #realtime time
+#print(nasdaq.Symbol[7])
 
+time = print() #realtime time
+smbls = 8576
 #http://pandas.pydata.org/pandas-docs/version/0.12/io.html
 def nasdaq():
-    print()
+    print("...Process Started...")
+    print("...Getting NASDAQ File...")
+    nasdaq_file = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
+    nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"])
+    print("...Got NASDAQ File...")
+
+    print("...Getting",str(smbls),"Quotes...")
+    for i in smbls:
+        print()
     #market = stock_market
