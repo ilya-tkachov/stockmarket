59eabf92c649045460cc019cf8a9420233df61cd
ilyat <ilya.tkachov@hotmail.com>
2018-06-09 17:01:23 -0400

    Add files via upload

--- class_getindex.py
+++ class_getindex.py
@@ -1,16 +1,17 @@
 import pandas
 import os
+import random
 import pandas_datareader
 from class_stockdata import *
 #from pandas_datareader import data, wb
 from datetime import datetime
+from pathlib import Path
 
-nasdaqfile = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
-nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"])
+#nasdaqfile = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
+#nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"])
 #pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(path_or_buf="E:\Comp Sci\final project\_index\nasdaq\symbols.txt")
-shr = "NVDA"
-a = pandas_datareader.robinhood.RobinhoodQuoteReader({shr}).read().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),header=False,index=False)#str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),cols="last_trade_price")#header=False,index=False)
-b = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),index_col=0) #usecols=['last_trade_price','updated_at']
+#a = pandas_datareader.robinhood.RobinhoodQuoteReader({shr}).read().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),header=False,index=False)#str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),cols="last_trade_price")#header=False,index=False)
+#b = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv"),index_col=0) #usecols=['last_trade_price','updated_at']
 #pandas.to_string -------<<<<<<<< USE INSTEAD AT http://pandas.pydata.org/pandas-docs/version/0.12/io.html
 
 #print(b.index[7])
@@ -19,15 +20,74 @@ b = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(shr)+".csv
 
 time = print() #realtime time
 smbls = 8576
-#http://pandas.pydata.org/pandas-docs/version/0.12/io.html
-def nasdaq():
-    print("...Process Started...")
-    print("...Getting NASDAQ File...")
-    nasdaq_file = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
-    nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"])
-    print("...Got NASDAQ File...")
+exclude = ["atest","ntest", "test", "z", "cbx",".","$","HJLI"]
 
-    print("...Getting",str(smbls),"Quotes...")
-    for i in smbls:
-        print()
-    #market = stock_market
+#http://pandas.pydata.org/pandas-docs/version/0.12/io.html
+def generate_numbers(smbls):
+    lst = []
+    s = 10
+    print("\n...Getting Quotes")
+    for i in range(0,s):
+        g = random.randrange(0,smbls)
+        if str(nasdaq.Symbol[g]).isalpha() == False:
+            print("...Cannot get",str(nasdaq.Symbol[g]))
+            s+=1
+        else:
+            lst.append(g)
+    print("...Got Quotes:")
+    for v in lst:
+        print(str(v),";",str(nasdaq.Symbol[v]))
+    return lst
+
+def f_nasdaq():
+    global nasdaq
+    global shares
+    print("...Process Started...")
+    print("...Checking NASDAQ File...")
+    if Path(os.getcwd()+"\\_index\\nasdaq\\symbols.txt").exists():
+        print("...Found NASDAQ File, Skipping...")
+    else:
+        print("...Cannot Find File, Getting NASDAQ...")
+        nasdaq_file = pandas_datareader.nasdaq_trader.get_nasdaq_symbols().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"))
+        print("...Got NASDAQ File...")
+    nasdaq = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\symbols.txt"),usecols=["Symbol"],converters={"$":""})
+    shares = generate_numbers(smbls)
+    for i in shares:
+        pandas_datareader.robinhood.RobinhoodQuoteReader(str(nasdaq.Symbol[i])).read().to_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(nasdaq.Symbol[i])+".csv"),header=False,index=False)
+        print("...Retrieved",str(nasdaq.Symbol[i]))
+    
+def market():
+    data = []
+    hist = []
+    print("\n...Creating Stock Market...")
+    for i in shares:
+        print("\n...Adding",str(nasdaq.Symbol[i]),"To Stock Market")
+        sx = pandas.read_csv(str(os.getcwd()+"\\_index\\nasdaq\\_shares\\"+str(nasdaq.Symbol[i])+".csv"),index_col=0)
+        temp_stock = accum_stock(str(nasdaq.Symbol[i]),sx.index[7],sx.index[12])
+        print("......Fetching Historical Data...")
+        dx = pandas_datareader.DataReader(str(nasdaq.Symbol[i]), 'robinhood')
+        print("......Fetched Historical Data...")
+        print(".........Adding Yearly Data...")
+        max_range = len(dx.head().index.levels[1])
+        for indx in range(0,max_range):
+            date = dx.head(max_range).index.levels[1][indx].date()
+            open_price = dx.head(max_range).get_value(dx.head(max_range).index[indx],"open_price")
+            close_price = dx.head(max_range).get_value(dx.head(max_range).index[indx],"close_price")
+            volume = dx.head(max_range).get_value(dx.head(max_range).index[indx],"volume")
+            temp_hist = stock_data(date,open_price,close_price,volume)
+            temp_stock.add_data(temp_hist)
+        print(".........Fet ched",temp_stock.data[0],";",temp_stock.data[max_range-1])
+        data.append(temp_stock)
+        print("......Added Yearly Data Successfully...")
+        #len(dx.head().index.levels[1]) --length
+        #dx.head().index.levels[1][0].date() ---time
+        #dx.head().get_value(dx.head().index[0],"open_price") ----close_price, open_price, volume
+    m = stock_market(data)
+    print("\n...Created Stock Market Successfully...")
+    return m
+    
+f_nasdaq()
+m = market()
+
+#m.fetch("NTEC").pull_history(18,7,2017)
+    
--- class_stockdata.py
+++ class_stockdata.py
@@ -1,4 +1,6 @@
 import pygame
+from datetime import datetime
+#from datetime import datetime
 pygame.init()
 
 #game works 
@@ -11,34 +13,43 @@ pygame.init()
 class stock_market(object):
     def __init__(self,data):
         self.data = data
-        self.stocks = []
     #give this class ability to add a whole new stock, its properties and add values to that stock
     def add_stock(self,stock):
         self.stocks.append(stock)
 
-    def display(self):
-        for i in stocks:
-            print("\nN:",i,"DB-C:",str(i.data.len()))
-            for g in i.data: 
-                print("\tDATE:",g.name,"OPEN:",g.val_open,"CLOSE:",g.val_close,"VOL",g.volume)
+    def fetch(self,name):
+        for i in self.data:
+            if i.ticker == name:
+                return i
+
+##    def display(self):
+##        for i in self.data:
+##            print("\nN:",i,"DB-C:",str(i.data.len()))
+##            for g in i.data: 
+##                print("\tDATE:",g.name,"OPEN:",g.val_open,"CLOSE:",g.val_close,"VOL",g.volume)
 
 #individual stock generation class
 #
 
 class accum_stock(object):
-    def __init__(self,ticker,rt_price,rt_vol,rt_time):
+    def __init__(self,ticker,rt_price,rt_time):
         self.ticker = ticker
         self.rt_price = rt_price
-        self.rt_vol = rt_vol
         self.rt_time = rt_time
         self.data = []
         
     def add_data(self,data):
         self.data.append(data)
+
+    def price(self):
+        return self.rt_price
+
+    def time(self):
+        return self.rt_time
     
-    def fetch(self,date):
+    def pull_history(self,d,m,y):
         for i in self.data:
-            if i.name == date:
+            if i.date == datetime.date(datetime(y,m,d)):
                 return i
 
     def __str__(self):
@@ -46,14 +57,14 @@ class accum_stock(object):
     __repr__ = __str__
         
 class stock_data(accum_stock): #ability to retrieve database prices
-    def __init__(self,date_time,val_open,val_close,volume): 
-        self.name = date_time
-        self.val_open = val_open
-        self.val_close = val_close
-        self.volume = volume        
-                      
+    def __init__(self,date,open_price,close_price,volume): 
+        self.date = date
+        self.open_price = open_price
+        self.close_price = close_price
+        self.volume = volume
+        
     def __str__(self):
-        return self.name
+        return self.date.strftime("%B %d, %Y")
     __repr__ = __str__
 
         
