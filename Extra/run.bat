380cc56a1668f36d711a9478e62e327dcbd41517
ilyat <ilya.tkachov@hotmail.com>
2018-06-13 13:51:59 -0400

    Add files via upload

---
+++ Extra/run.bat
@@ -0,0 +1,14 @@
+cd C:\Python34
+python -m pip install -U pip
+cd C:\Python34\Scripts
+pip install numpy
+pip install pandas==0.19.2
+pip install lxml
+pip install requests --upgrade
+pip install requests-file
+pip install requests-ftp
+pip install wrapt
+pip install pandas-datareader
+pip install quandl
+pip install html5lib
+pip install BeautifulSoup4
